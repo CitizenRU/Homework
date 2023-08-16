0049a84437cbbea53375470168b18b0145a4e579
Igor <krutnevi@bk.ru>
2023-08-16 16:42:23 +0300

    Задача 4

---
+++ homework_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int count1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int count2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int count3 = Convert.ToInt32(Console.ReadLine());
+int max = count1;
+if (max < count2) max = count2;
+if (max < count3) max = count3;
+
+System.Console.WriteLine("Максимальное из этих чисел = " + max);
