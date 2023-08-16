ec3b7e7ece6e4aa67c40b2b0b42342146252e22f
Igor <krutnevi@bk.ru>
2023-08-16 16:07:37 +0300

    Задача 2

---
+++ homework_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int count1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int count2 = Convert.ToInt32(Console.ReadLine());
+
+if (count1 > count2)
+{
+    System.Console.WriteLine("Большее число: " + count1 + "Меньшее число: " + count2);
+}
+
+else
+{
+    System.Console.WriteLine("Большее число: " + count1 + "Меньшее число: " + count2);
+} 
